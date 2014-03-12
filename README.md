eaf00b07fa7783e658591e4d4f49e65ca9e1dfe4
Adriano Di Giovanni <me@adrianodigiovanni.com>
2014-03-12 17:15:43 +0100

    v0.1.0

--- README.md
+++ README.md
@@ -1,4 +1,47 @@
-node-error
-==========
+# Error
 
-A library of extendable custom errors for node.js
+Error is a library of extendable custom errors for node.js.
+
+## Installation
+
+```
+npm node-error --save
+```
+
+## Usage
+
+Errors in the library can be instantiated directly
+
+```javascript
+var
+	LoggableError = require('node-error').LoggableError;
+
+throw new LoggableError('message');
+```
+
+or extended using `.extend(name, [attributes])`:
+
+```javascript
+var
+	LoggableError = require('node-error').LoggableError;
+
+var
+	attributes = function () {
+		return {
+			memoryUsage: process.memoryUsage()
+		};
+	},
+	CustomError = LoggableError.extend('CustomError', attributes);
+```
+
+The `attributes` argument to the `extend` method is optional.
+It can be of type object or function.
+The function executes upon error instantiation and it should return an object.
+
+```javascript
+var
+	error = new CustomError('message');
+
+console.log(error.attributes);
+```
+
---
+++ test/index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var
+  path = require('path'),
+  assert = require('assert');
+
+var
+  basePath = process.cwd(),
+  equal = assert.equal,
+
+  LoggableError = require(path.resolve(basePath+'/lib/error')).LoggableError;
+
+describe('LoggableError', function () {
+  it('instantiate', function () {
+    var
+      error = new LoggableError();
+
+    equal(error.name, 'LoggableError');
+    equal(error.propertyIsEnumerable('stack'), true);
+  });
+});
+
+describe('LoggableError subclass', function () {
+  var
+    version = require(path.resolve(basePath+'/package')).version;
+
+  it('extend without attributes and instantiate', function () {
+    var
+      SubError = LoggableError.extend('SubError'),
+      error = new SubError();
+
+    equal(error.name, 'SubError');
+    equal(error instanceof SubError, true);
+  });
+
+  it('extend with attributes hash and instantiate', function () {
+    var
+      attributes = { version: version },
+      SubError = LoggableError.extend('SubError', attributes),
+      error = new SubError();
+
+    assert.deepEqual(error.attributes, attributes);
+  });
+
+  it('extend with attributes function and instantiate', function () {
+    var
+      memoryUsage = process.memoryUsage(),
+      attributes = function () {
+        return {
+          version: version,
+          memoryUsage: memoryUsage
+        };
+      },
+      SubError = LoggableError.extend('SubError', attributes),
+      error = new SubError();
+
+    equal(error.attributes.version, version);
+    assert.deepEqual(error.attributes.memoryUsage, memoryUsage);
+  });
+});
